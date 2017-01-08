dc2c0afab4b05ce404a71c4b5fe67fe5ee9dbda8
LUOHONGRUI <hongruiluo@hotmail.com>
2017-01-08 21:11:18 +0800

    optimize multi drop in one page

--- dist/dropload.js
+++ dist/dropload.js
@@ -56,7 +56,9 @@
 
         // 如果加载下方，事先在下方插入DOM
         if(me.opts.loadDownFn != ''){
-            me.$element.append('<div class="'+me.opts.domDown.domClass+'">'+me.opts.domDown.domRefresh+'</div>');
+            if ($('.' + me.opts.domDown.domClass, me.$element).length <= 0) {
+                me.$element.append('<div class="'+me.opts.domDown.domClass+'">'+me.opts.domDown.domRefresh+'</div>');
+            }
             me.$domDown = $('.'+me.opts.domDown.domClass);
         }
 
